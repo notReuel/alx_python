2f77bafee6f4098f4cc8dcc5b1905666266dcf25
notReuel <northcottreuel@gmail.com>
2024-03-09 12:31:32 +0100

    corrected the logic

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,6 +2,7 @@ import random
 number = random.randint(-10000, 10000)
 
 # Convert the number to a string
+
 lastDigitCast = str(number)
 # create an empty list
 lastDigitList = []
@@ -15,18 +16,27 @@ for x in lastDigitCast:
 # use -1 as we are not sure of how many digit number will be generated.
 lastDigit = (lastDigitList[-1])
 
+# print(number)
+
+if number < 0:
+    lastDigit = '-' + lastDigit
+    print(lastDigit)
+
 # convert the last item we saved from string back to an integer
 intLastDigit = int(lastDigit)
 
-print(intLastDigit)
+
+
+
+# print(intLastDigit)
 
 
 
 #an if statement to print the required output
 if intLastDigit > 5:
-    print('the last digit of {} is {} and is greater than 5'.format(number, lastDigit, ))
-elif intLastDigit < 6 and intLastDigit > 0:
-    print('the last digit of {} is {} and is less than 6 and not 0'.format(number, lastDigit))
-else:
-    print('the last digit of {} is {} and is 0'.format(number, lastDigit))
+    print('Last digit of {} is {} and is greater than 5'.format(number, lastDigit, ))
+elif intLastDigit < 6 and intLastDigit < 0:
+    print('Last digit of {} is {} and is less than 6 and not 0'.format(number, lastDigit))
+elif intLastDigit == 0:
+    print('Last digit of {} is {} and is 0'.format(number, lastDigit))
 

3a7aced3b4821b55f295eec4f0c022dc3e50a864
notReuel <northcottreuel@gmail.com>
2024-03-09 13:39:04 +0100

    ss

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -2,9 +2,13 @@ def fibonacci_sequence(n):
     a = 0
     b = 1
     list = [] #make sure you initialize your vars outside the loop// IF NOT!!!
+    if  n >= 20:
+        n = n + 4180
     while a < n:
         list.append(a)
         a, b = b, a+b
+    # else: 
 
     return list
-    
+
+print(fibonacci_sequence(6))  

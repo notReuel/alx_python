6af4eed259cc1c440af647af48031f695210f2f2
notReuel <northcottreuel@gmail.com>
2024-03-09 22:21:12 +0100

    best score

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,21 +1,25 @@
 def best_score(a_dictionary):
     highest_score = (0)  # Initialize with zero
     best_key = None
-
-    for name, score in a_dictionary.items():
-        if score > highest_score:
-            highest_score = score
-            best_key = name
+    
+    if a_dictionary == None:
+        best_key = None
+    else:
+        for name, score in a_dictionary.items():
+            if score > highest_score:
+                highest_score = score
+                best_key = name
            
-    return best_key, highest_score
+        return best_key
 
 # Example usage:
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
+if __name__=="__main__":
+    a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+    best_key = best_score(a_dictionary)
+    print("Best score: {}".format(best_key))
 
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
+    best_key = best_score(None)
+    print("Best score: {}".format(best_key))
 
 # highest_name, highest_score = best_score(a_dictionary)
 # print(f"The highest score is {highest_score} by {highest_name}")

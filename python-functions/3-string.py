279c301b3f7418d4b709f1a69bcfcd238c8a763c
notReuel <northcottreuel@gmail.com>
2024-03-09 13:27:57 +0100

    added the join method to return the correct output, instead of hexvalues

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,7 +1,4 @@
 def reverse_string(string):
-    return reversed(string)
+    return ''.join(reversed(string))
     
-print(reverse_string('Hello'))
-print(reverse_string(""))
-print(reverse_string("madam"))
-print(reverse_string("Hello, World"))
+

4c3ba7c9583ab46c8459c5d409b0a8497dd83961
Northcott Reuel <155072763+notReuel@users.noreply.github.com>
2024-03-09 13:14:36 +0100

    Update 2-temperature.py
    updated the logic

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,9 +1,4 @@
 def convert_to_celcius(fahrenheit):
-    convCel = fahrenheit - 180
-    print(convCel)
-    return convCel
-
-(convert_to_celcius(100))
-# print(convert_to_celcius(-40))
-# print(convert_to_celcius(-459.67))
-# print(convert_to_celcius(32))
+    celsius = (5/9) * (fahrenheit - 32)
+    print(celsius)
+    return celsius

b3ffd33c7c736373fea6454768646adbb36bf311
notReuel <northcottreuel@gmail.com>
2024-03-09 12:13:36 +0100

    removed all the new lines. they were not required

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -4,8 +4,8 @@ number = random.randint(-10, 10)
 if number > 0:
     print('{} is positive'.format(number))
 elif number < 0:
-    print('{} is negative\n'.format(number))
+    print('{} is negative'.format(number))
 else:
-    print('{} is zero\n'.format(number))
+    print('{} is zero'.format(number))
     
     

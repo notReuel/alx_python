4743ec797c7c8813123ee0a7edd34b157bcb38ca
notReuel <northcottreuel@gmail.com>
2024-03-09 12:11:23 +0100

    removed an extra white space

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -2,10 +2,10 @@ import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
 if number > 0:
-    print('{} is positive \n'.format(number))
+    print('{} is positive\n'.format(number))
 elif number < 0:
-    print('{} is negative \n'.format(number))
+    print('{} is negative\n'.format(number))
 else:
-    print('{} is zero \n'.format(number))
+    print('{} is zero\n'.format(number))
     
     

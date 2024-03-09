15902ca1e59bd78c576385467a52c6404d0241a4
notReuel <northcottreuel@gmail.com>
2024-03-09 11:43:01 +0100

    adjusted the 1-edges to work universal regardless of values

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,8 @@
-word = "Holberton"
+word = "School"
+
 #the first 3 letters 
 print("First 3 letters: {}".format(word[:3]))
 # The last 2 letters
-print("Last 2 letters: {}".format(word[7:]))
+print("Last 2 letters: {}".format(word[-2:]))
 #The middle word, excluding the first and last characters
-print("Middle word: {}".format(word[1:8]))
+print("Middle word: {}".format(word[1:-1]))

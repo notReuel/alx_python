13e5dc2b82d386744ba306a05aa1646c463bc345
notReuel <northcottreuel@gmail.com>
2024-03-09 16:33:05 +0100

    new commit

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -6,7 +6,6 @@ def multiple_returns(sentence):
     return length, first
 
 if __name__ == "__main__":
-    sentence = "At Holberton school, I learnt C!"
+    sentence = ""
     length, first = multiple_returns(sentence)
     print("Length: {:d} - First character: {}".format(length, first))
-    

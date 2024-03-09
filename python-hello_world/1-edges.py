1fea4331c2d490ba7f3b8900d28cc00f787d1018
notReuel <northcottreuel@gmail.com>
2024-03-09 11:54:12 +0100

    another commit

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
-word = "School"
+word = input("Enter Word: ")
 print("First 3 letters: {}".format(word[:3]))
 print("Last 2 letters: {}".format(word[-2:]))
 print("Middle word: {}".format(word[1:-1]))
-word2 = "Holberton"
-print("First 3 letters: {}".format(word2[:3]))
-print("Last 2 letters: {}".format(word2[-2:]))
-print("Middle word: {}".format(word2[1:-1]))
+# word2 = "Holberton"
+# print("First 3 letters: {}".format(word2[:3]))
+# print("Last 2 letters: {}".format(word2[-2:]))
+# print("Middle word: {}".format(word2[1:-1]))

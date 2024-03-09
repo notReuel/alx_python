06c9cf459c98a54d32106d1deab5bc49ffb871d8
Northcott Reuel <155072763+notReuel@users.noreply.github.com>
2024-03-09 13:24:22 +0100

    Update 2-temperature.py
    added a conditional statement to round up the numbers after the  decimal point for negative values, i.e -468.34

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,3 +1,9 @@
 def convert_to_celsius(fahrenheit):
-    celsius = (5/9) * (fahrenheit - 32)
-    return celsius
+    celcius = (5/9) * (fahrenheit - 32)
+    if fahrenheit < 0:
+        trimmedCelcius = round(celcius, 2)
+        return trimmedCelcius
+    else:
+        return celcius
+
+print(convert_to_celsius(-450))

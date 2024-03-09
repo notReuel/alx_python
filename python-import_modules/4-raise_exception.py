613f48bed4663196abee92908ee442a1cd0dec9d
notReuel <northcottreuel@gmail.com>
2024-03-09 16:02:29 +0100

    bb

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,7 +1,18 @@
-def raise_exception(a, b):
-    raise TypeError("Exception has been raised")    
+# def raise_exception():
+#     raise TypeError("Exception has been raised")    
     
+# try:
+#     raise_exception()
+# except TypeError as te:
+#     print("Exception raised")
+
+class MyCustomTypeError(Exception):
+    pass
+
+def raise_exception():
+    raise MyCustomTypeError("Exception has been raised")
+
 try:
     raise_exception()
-except TypeError as te:
-    print("Exception raised")
+except MyCustomTypeError as te:
+    print("Exception raised:", te)

1a730534fd0fa89331e71a0bc7e18fd7d521ebe9
notReuel <northcottreuel@gmail.com>
2024-03-26 23:30:02 +0100

    select states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+import MySQLdb
+from sys import argv
+
+mysql_username = argv[1]
+mysql_password = argv[2]
+mysql_database = argv[3]
+
+dbconnect = MySQLdb.connect(host='localhost', user=mysql_username, passwd=mysql_password, db=mysql_database)
+
+cursor = dbconnect.cursor()
+
+cursor.execute('SELECT * FROM states')
+states = cursor.fetchall()
+for state in states:
+    print(state)

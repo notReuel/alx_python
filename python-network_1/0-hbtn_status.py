c663526da594d1ed3533a3b702393cf3a84982e5
notReuel <northcottreuel@gmail.com>
2024-03-26 13:54:51 +0100

    added a tab escape sequence to the output of the request

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,6 +3,6 @@ import requests
 r = requests.get('https://alu-intranet.hbtn.io/status')
 
 print("Body response:")
-print(" - type: {}".format(type(r.text)))
-print(" - content: {}".format(r.text))
+print("\t- type: {}".format(type(r.text)))
+print("\t- content: {}".format(r.text))
 

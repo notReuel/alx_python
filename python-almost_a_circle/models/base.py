9bbd53ae271b1402e369abc0ef7f7440aaa9be7b
notReuel <northcottreuel@gmail.com>
2024-03-26 10:59:18 +0100

    rearranged

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,38 +1,36 @@
 class Base:
     '''
-    A private class with a variable nb_objects initialized to 0
+    This class will be the 'base' of all other classes
     '''
     __nb_objects = 0
-
     def __init__(self, id=None):
         '''
         defining the process of the function
         '''
-        # self.id = id
 
-        if id == None:
+        if id:
             '''
             a conditional statement
-            '''
+            '''            
+            self.id = id
+        else:
             Base.__nb_objects = Base.__nb_objects + 1
             self.id = Base.__nb_objects
-        else:
-            self.id = id
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     b1 = Base()
-#     print(b1.id)
+    b1 = Base()
+    print(b1.id)
 
-#     b2 = Base()
-#     print(b2.id)
+    b2 = Base()
+    print(b2.id)
 
-#     b3 = Base()
-#     print(b3.id)
+    b3 = Base()
+    print(b3.id)
 
-#     b4 = Base(12)
-#     print(b4.id)
+    b4 = Base(12)
+    print(b4.id)
 
-#     b5 = Base()
-#     print(b5.id)
+    b5 = Base()
+    print(b5.id)

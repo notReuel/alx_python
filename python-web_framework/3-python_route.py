86b491669d4534620910304defaeb9ae67827252
notReuel <northcottreuel@gmail.com>
2024-03-27 11:12:56 +0100

    PythonisCool - v1.0.0

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,31 @@
+"""FLASK initialized"""
+from flask import Flask
+
+app = Flask(__name__)
+
+"""
+    Route the app
+"""
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'    
+
+@app.route('/c/<text>', strict_slashes=False)
+def CisFun(text):
+    formatted_text = text.replace('_', ' ')
+    return 'C {}'.format(formatted_text)
+
+@app.route('/python/<text>', strict_slashes=False)
+def pythonIsFun(text):
+    formatted_text = text.replace('_', ' ')
+    return 'Python {}'.format(formatted_text)
+
+
+if __name__ == '__main__':
+    # port and host to run flask app
+    app.run(host='0.0.0.0', port=5000)
+

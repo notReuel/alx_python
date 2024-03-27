1886213d2d624726f0f22d322c9b6fc0c6552945
notReuel <northcottreuel@gmail.com>
2024-03-27 07:10:43 +0100

    another trail

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,9 +3,7 @@ def print_matrix_integer(matrix=[[]]):
         print()
         return
     for i in range(len(matrix)):
-        for j in range(len(matrix[i]-1)):
-            print("{:d}".format(matrix[i][j]), end=" ")
+        for j in range(len(matrix[i])-1):
+            print("{:d}".format(matrix[i][j]),end=' ')
         print("{:d}".format(matrix[i][-1]))
-    
-
-
+        

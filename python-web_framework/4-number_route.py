85dc02b1be4f902648afd411f819d55382b16e8a
notReuel <northcottreuel@gmail.com>
2024-03-27 13:47:40 +0100

    is it number v1.0.2

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -25,12 +25,12 @@ def pythonIsFun(text='is cool'):
     formatted_text = text.replace('_', ' ')
     return 'Python {}'.format(formatted_text)
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def num(n):
     if n is isinstance(n, int):
         return n
     else: 
-        return "n is anumber                                                                           "
+        return "{} is a number".format(n)                                                                           "
 
 
 if __name__ == '__main__':

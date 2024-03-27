b310f79ee38a79865276de0f26a228494fccd249
notReuel <northcottreuel@gmail.com>
2024-03-27 08:20:43 +0100

    used a %s in place of ${}

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,7 +9,7 @@ stateName = argv[4]
 dbconnect = MySQLdb.connect(host='localhost', port=3306, user=mysql_username, passwd=mysql_password, db=mysql_database)
 cursor = dbconnect.cursor()
 
-cursor.execute("SELECT FROM states WHERE BINARY name = '{}' ".format(stateName))
+cursor.execute("SELECT FROM states WHERE BINARY name  = $s ", stateName)
 states = cursor.fetchall()
 for state in states:
     print(state)

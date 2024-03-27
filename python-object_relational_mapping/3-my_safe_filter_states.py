f1a7f20ac388d3aa1f21da9391cfc7e5815a1c7d
notReuel <northcottreuel@gmail.com>
2024-03-27 08:18:55 +0100

    safe filter with format()

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+import MySQLdb
+from sys import argv
+
+mysql_username = argv[1]
+mysql_password = argv[2]
+mysql_database = argv[3]
+stateName = argv[4]
+
+dbconnect = MySQLdb.connect(host='localhost', port=3306, user=mysql_username, passwd=mysql_password, db=mysql_database)
+cursor = dbconnect.cursor()
+
+cursor.execute("SELECT FROM states WHERE BINARY name = '{}' ".format(stateName))
+states = cursor.fetchall()
+for state in states:
+    print(state)
+
+# close cursor
+cursor.close()
+# close database
+dbconnect.close()

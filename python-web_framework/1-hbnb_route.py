5ae1e22644357e0d1e2de7929681505e0a48baef
notReuel <northcottreuel@gmail.com>
2024-03-27 10:47:19 +0100

    HBNB - v1.0.0

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,20 @@
+"""FLASK initialized"""
+from flask import Flask
+
+app = Flask(__name__)
+
+"""
+    Route the app
+"""
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'    
+
+if __name__ == '__main__':
+    # port and host to run flask app
+    app.run(host='0.0.0.0', port=5000)
+

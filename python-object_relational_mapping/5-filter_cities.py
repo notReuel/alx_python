679d876c8f826f8dc555c1489c6fdca5c60f3ad4
notReuel <northcottreuel@gmail.com>
2024-03-27 09:14:09 +0100

    filter cities -v1.0.0

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+import MySQLdb
+from sys import argv
+
+mysql_username = argv[1]
+mysql_password = argv[2]
+mysql_database = argv[3]
+state_Name = argv[4]
+
+dbconnect = MySQLdb.connect(host='localhost', port=3306, user=mysql_username, passwd=mysql_password, db=mysql_database)
+cursor = dbconnect.fetchall()
+
+cursor.execute("SELECT name FROM cities WHERE state_id = (SELECT id FROM states WHERE BINARY name = %s) ",[state_Name])
+cities = cursor.fetchall()
+
+for i, city in enumerate(cities):
+    print(city[0], end=', ' if i < len(cities) -1 else'\n')
+if not cities:
+    print('')
+
+cursor.close()
+dbconnect.close()

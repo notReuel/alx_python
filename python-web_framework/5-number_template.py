496840374064ee4dee7feeae960693f4c524f587
notReuel <northcottreuel@gmail.com>
2024-03-27 14:54:29 +0100

    5-number v1.0.4

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -1,61 +1,42 @@
 """FLASK initialized"""
-from flask import Flask
+from flask import Flask, render_template
+from markupsafe import escape
 
 app = Flask(__name__)
 
-"""
-    Route the app
-"""
-@app.route('/', strict_slashes=False)
+"""create route at / display --> “Hello HBNB!”"""
+@app.route('/',strict_slashes=False)
 def hello():
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
-"""
-route to display HBNB
-"""
-@app.route('/hbnb', strict_slashes=False)
+"""create route at /hbnb display --> “HBNB”"""
+@app.route('/hbnb',strict_slashes=False)
 def hbnb():
-    return 'HBNB'    
+    return "HBNB"
 
-"""
-to display C is fun
-"""
-@app.route('/c/<text>', strict_slashes=False)
-def CisFun(text):
-    formatted_text = text.replace('_', ' ')
-    return 'C {}'.format(formatted_text)
+"""create route at /c/<text> display -->  “C ” followed by the value of the text """
+@app.route('/c/<text>',strict_slashes=False)
+def c(text):
+    text=text.replace('_', ' ',)
+    return f"C {escape(text)}"
 
-"""
-for Python is cool
-"""
-@app.route('/python/<text>', strict_slashes=False)
-@app.route('/python', strict_slashes=False)
-def pythonIsFun(text='is cool'):
-    formatted_text = text.replace('_', ' ')
-    return 'Python {}'.format(formatted_text)
+"""create route at /python/<text> display -->  “python ” followed by the value of the text defult 'is cool' """
+@app.route('/python/<text>',strict_slashes=False)
+@app.route('/python',strict_slashes=False)
+def python(text="is cool"):
+    text=text.replace('_', ' ',)
+    return f"Python {escape(text)}"
 
-"""
-create route at /number/<n> display --> “n is a number” if n is a number
-"""
+"""create route at /number/<n> display --> “n is a number” if n is a number"""
 @app.route('/number/<int:n>',strict_slashes=False)
 def number(n):
     return f"{escape(n)} is a number"
-                                                                          
-"""
-create route at /number/<n> display --> “n is a number” if n is a number
-"""
+
+"""create route at /number_template/<n> render html """
 @app.route('/number_template/<int:n>',strict_slashes=False)
-def number(n):
-    """
-    opens the html page
-    """
+def number_template(n):
     return render_template('5-number.html', num=n)
 
-
-"""
-run the program
-"""
 if __name__ == '__main__':
-    # port and host to run flask app
+    '''run flask on 0.0.0.0, port 5000'''
     app.run(host='0.0.0.0', port=5000)
-

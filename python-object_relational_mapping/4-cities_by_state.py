2d7b2b4cec7b93de5237c46821aa8087917f1d3a
notReuel <northcottreuel@gmail.com>
2024-03-27 08:37:58 +0100

    cities by state - version 1.0.0 (hopefully the last)

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+import MySQLdb
+from sys import argv
+
+mysql_username = argv[1]
+mysql_password = argv[2]
+mysql_database = argv[3]
+
+dbconnect = MySQLdb.connect(host = 'localhost', port=3306, user=mysql_username, passwd=mysql_password, db=mysql_database)
+cursor = dbconnect.cursor()
+
+cursor.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states = cities.states_id")
+cities = cursor.fetchall()
+
+for city in cities:
+    print (city)
+
+cursor.close()
+dbconnect.close()
+

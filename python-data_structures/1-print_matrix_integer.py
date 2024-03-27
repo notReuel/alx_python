e06e8da0ef076bf947319abe3aac74f76ba19aa6
notReuel <northcottreuel@gmail.com>
2024-03-27 07:08:15 +0100

    changed the logic

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,22 +1,16 @@
-def print_matrix_integer(matrix=None):
-    if matrix is None:
-        print("$")
+def print_matrix_integer(matrix=[[]]):
+    if not any(matrix):
+        print()
         return
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i]-1)):
+            print("{:d}".format(matrix[i][j]), end=" ")
+        print("{:d}".format(matrix[i][-1]))
     
-    if isinstance(matrix, str):   #If input is a string, just print it
-        print("{}$".format(matrix))
-        return
-    
-    for row in matrix:
-        formattedRow = ' '.join(str(item) for item in row)
-        print("{}$".format(formattedRow))
 
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]
-if __name__ == "__main__":
-    print_matrix_integer(matrix)
-    print_matrix_integer("--")
-    print_matrix_integer()
+

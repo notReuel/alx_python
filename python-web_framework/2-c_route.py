c07be4b3ff4d19098890bd0171ef1a5f5049dab4
notReuel <northcottreuel@gmail.com>
2024-03-27 10:55:13 +0100

    CisFun - v1.0.0

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,25 @@
+"""FLASK initialized"""
+from flask import Flask
+
+app = Flask(__name__)
+
+"""
+    Route the app
+"""
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'    
+
+@app.route('/c/{}'.format(), strict_slashes=False)
+def CisFun(text):
+    return 'C {}'.format(text)
+
+
+if __name__ == '__main__':
+    # port and host to run flask app
+    app.run(host='0.0.0.0', port=5000)
+

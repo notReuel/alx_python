9ac68217290b7bcd533ec02a02552f02cf7076ce
notReuel <northcottreuel@gmail.com>
2024-03-27 14:52:01 +0100

    5-number - v1.0.1 ---- added documentation

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -10,10 +10,16 @@ app = Flask(__name__)
 def hello():
     return 'Hello HBNB!'
 
+"""
+route to display HBNB
+"""
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'    
 
+"""
+to display C is fun
+"""
 @app.route('/c/<text>', strict_slashes=False)
 def CisFun(text):
     formatted_text = text.replace('_', ' ')
@@ -28,16 +34,24 @@ def pythonIsFun(text='is cool'):
     formatted_text = text.replace('_', ' ')
     return 'Python {}'.format(formatted_text)
 
-"""create route at /number/<n> display --> “n is a number” if n is a number"""
+"""
+create route at /number/<n> display --> “n is a number” if n is a number
+"""
 @app.route('/number/<int:n>',strict_slashes=False)
 def number(n):
     return f"{escape(n)} is a number"
                                                                           
-"""create route at /number/<n> display --> “n is a number” if n is a number"""
+"""
+create route at /number/<n> display --> “n is a number” if n is a number
+"""
 @app.route('/number_template/<int:n>',strict_slashes=False)
 def number(n):
     return render_template('5-number.html', num=n)
 
+
+"""
+run the program
+"""
 if __name__ == '__main__':
     # port and host to run flask app
     app.run(host='0.0.0.0', port=5000)

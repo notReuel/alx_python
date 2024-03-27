2c2eaf8a41849fd1fb9cce5b7be9a9c132410d1e
notReuel <northcottreuel@gmail.com>
2024-03-27 09:49:49 +0100

    fetch states first - v1.0.0

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base 
+from sqlalchemy import create_engine
+from sys import argv
+
+mysql_username = argv[1]
+mysql_password = argv[2]
+mysql_database = argv[3]
+
+path = "mysql+mysqldb://{}:{}@localhost/{}".format(mysql_username, mysql_password, mysql_database)
+
+database = create_engine(path)
+Session = sessionmaker(bind=database)
+session = Session()
+
+states = session.query(State).first()
+
+if states:
+    print("{}: {}".format(state.id, state.name))
+else: 
+    print("Nothing")
+

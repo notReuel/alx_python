c6186eeeb70cfb717a5e4b673c22eaa14eb99c6f
notReuel <northcottreuel@gmail.com>
2024-03-27 07:39:03 +0100

    combination of two results

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,11 +1,8 @@
-for number in range(1, 10):
-    # if number == int(str(number)[::-1]):
-        print('{:02d}'.format(number), end=", ")
-    
-for number in range (11, 89):
-    # for numbers above 11, divide by 10 and print only 
-    # items with decimal point greater than zero, hence eliminating 20, 30, 40 etc.
-
-    if (int(str(number/10).split('.')[1])) > 0:
-        print('{:02d}'.format(number), end=", ")
-print(89)
+for i in range(0, 10):
+    for j in range(i+1,10):
+        if i!=j:
+            if str(i)+str(j) == '89':
+                print(89)
+            else:
+                print('{}{}'.format(i,j),end=', ')
+                

8306ea0aa39eb4fd14f6afd56c967e981b289335
notReuel <northcottreuel@gmail.com>
2024-03-27 07:28:16 +0100

    changed the logic flow

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,11 +1,8 @@
 def multiple_returns(sentence):
     length = len(sentence)
-    for char in sentence:
-        if char == sentence[0]:
-            first = char
+    if length == 0:
+        first = None
+    else: 
+        first = sentence[0]
     return length, first
 
-if __name__ == "__main__":
-    sentence = ""
-    length, first = multiple_returns(sentence)
-    print("Length: {:d} - First character: {}".format(length, first))

88fec4aaeebe1741b4453d9e3f11695884d794d7
notReuel <northcottreuel@gmail.com>
2024-03-28 07:58:54 +0100

    6-my github - v1.0.9

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,18 +1,19 @@
 import requests
 import sys
 
-URL = 'https://api.github.com/user/'
 
 if len(sys.argv) > 1:
     username = sys.argv[1]
     pwd = sys.argv[2]
 
-    try:
-        res = requests.get(URL, auth=(username,pwd))
-        res_json = res.json()
-        print(res_json['id'])
-    except Exception as e:
-        print('None')
+URL = 'https://api.github.com/user/'
+
+try:
+    res = requests.get(URL, auth=(username,pwd))
+    res_json = res.json()
+    print(res_json['id'])
+except Exception as e:
+    print('None')
     
 
 
